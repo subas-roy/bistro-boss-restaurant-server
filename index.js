e03257a56e0777d4e1fed6e127ac18bb8914b0c0
subas-roy <subasroy46@gmail.com>
2025-08-19 10:04:28 +0600

    create API endpoint to get all menu items

--- index.js
+++ index.js
@@ -23,13 +23,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await client.connect(); //  
+
+    // Get the 'menu' collection from the 'bistrodb' database
+    const menuCollection = client.db("bistrodb").collection("menu");
+
+    // API endpoint to get all menu items
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close(); // Uncomment this line to close the MongoDB connection when done (not recommended for persistent servers)
   }
 }
 run().catch(console.dir);
